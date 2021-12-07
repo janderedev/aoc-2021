255d3afc471f83de18f6ba05f5f54ddb0897dff6
JandereDev <me@janderedev.xyz>
2021-12-07 16:54:25 +0100

    day 7

---
+++ Day 7/1/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync('../input.txt', 'utf-8').split(',').map(i => Number(i));
+
+let highestNum = input.sort((a, b) => b - a)[0],
+    lowestNum  = input.sort((a, b) => a - b)[0];
+
+let bestPos, bestPosFuel;
+
+for (let i = lowestNum; i <= highestNum; i++) {
+    let fuelRequired = 0;
+    for (let pos of input) {
+        while (pos != i) {
+            if (pos > i) pos--; else pos++;
+            fuelRequired++;
+        }
+    }
+
+    if (!bestPos || bestPosFuel > fuelRequired) {
+        bestPos = i;
+        bestPosFuel = fuelRequired;
+    }
+}
+
+console.log(`Result: Position ${bestPos} with ${bestPosFuel} fuel`);
